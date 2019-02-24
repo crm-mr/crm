a7a3eaa04075f688e715ac933a69505a55055f20
Hugh Balboa <meridiano33@hotmail.com>
2019-02-23 23:13:15 -0600

    estilos
    cambios de login y css

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -10,14 +10,14 @@ const Header = () => {
             
             
 
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top navbar_scroll">
+            <nav className="navbar navbar-expand-lg   fixed-top navbar_scroll">
             <div className="container">
                 <Link to={'/'}><img className="img_logo_nav" src={logo_turn} /></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="fa fa-bars change_color_bars_menu"></span>
                 </button>
                 <div className="title_main_brand">
-                MARCA
+                 
                 </div>
             </div>
             </nav>
